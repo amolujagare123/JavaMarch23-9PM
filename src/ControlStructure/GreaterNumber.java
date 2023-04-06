9551c3bd4027c6a7be9bda0348b93007e54ea36b
amolu <amolujagare@gmail.com>
2023-04-06 20:49:01 +0530

    java

---
+++ src/ControlStructure/GreaterNumber.java
@@ -0,0 +1,21 @@
+package ControlStructure;
+
+public class GreaterNumber {
+
+    public static void main(String[] args) {
+
+        int a = 100;
+        int b = 20;
+
+        if(a>b) {
+            System.out.println("a is greater");
+            System.out.println("a=" + a);
+        }
+        else
+            System.out.println("b is greater");
+
+
+        System.out.println("b="+b);
+
+    }
+}
